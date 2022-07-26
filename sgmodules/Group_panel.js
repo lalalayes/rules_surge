215a3f6f433f4e1d5f83b5d0152240af39e8f59b
lalalayes <105582299+lalalayes@users.noreply.github.com>
2022-07-26 11:52:58 -0300

    Update Group_panel.js

--- sgmodules/Group_panel.js
+++ sgmodules/Group_panel.js
@@ -107,3 +107,11 @@ function getParams(param) {
       .map(([k, v]) => [k, decodeURIComponent(v)])
   );
 }
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
